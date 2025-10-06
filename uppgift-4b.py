2184a15820eab2ca28aba85d23ef643b31f0c985
tech.tech.2em2m <tech.tech.2em2m@slmail.me>
2025-10-06 09:38:15 +0200

    Upp3 commit

---
+++ uppgift-4b.py
@@ -0,0 +1,9 @@
+ålder = input("Skriv din åldersgrupp (0-6, 7-12, 13-17, 18+): ")
+if ålder == "0-6":
+    print("Du är för ung")
+elif ålder == "7-12":
+    print("Du är fortarande lite för ung")
+if ålder == "13-17":
+    print("Du får snart gammal nog")
+if ålder == "18+":
+    print("Du får skapa junior-konto.")
